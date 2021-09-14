c96abf4cc3b6683ac6ee95484f6ad7e49554ad56
prostagma-fp <83932623+prostagma-fp@users.noreply.github.com>
2021-09-14 00:07:09 -0300

    Wrap description in try

--- sites/DeviantArt.py
+++ sites/DeviantArt.py
@@ -22,20 +22,19 @@ class DeviantArt(Curation.Curation):
         # Get info and description
         try:
             originalDescription = str(soup.select(".XeBxZ > .legacy-journal")[0]).replace('\xa0', ' ')
+            replacements = [
+                (r'<a(.+?)href="(https:..www.deviantart.com.users.outgoing\?)?(.+?)"(.+?)>',  r'\3'),
+                (r'<img (.*?)alt="(.+?)"(.+?)\/>',  r'\2'),
+                (r'\s?<br\/?>', '\n'),
+                (r'\n?(<ul>)?<li>', '\n• '),
+                (r'<\/?(.+?)>', ''),
+                (r'\s?\n\n\n\s?', '\n\n')
+            ]
+            for old, new in replacements:
+                originalDescription = re.sub(old, new, originalDescription)
+            self.meta['originalDescription'] = originalDescription.strip('\n').strip()
         except:
             pass
-        print(originalDescription)
-        replacements = [
-            (r'<a(.+?)href="(https:..www.deviantart.com.users.outgoing\?)?(.+?)"(.+?)>',  r'\3'),
-            (r'<img (.*?)alt="(.+?)"(.+?)\/>',  r'\2'),
-            (r'\s?<br\/?>', '\n'),
-            (r'\n?(<ul>)?<li>', '\n• '),
-            (r'<\/?(.+?)>', ''),
-            (r'\s?\n\n\n\s?', '\n\n')
-        ]
-        for old, new in replacements:
-            originalDescription = re.sub(old, new, originalDescription)
-        self.meta['originalDescription'] = originalDescription.strip('\n').strip()
         
         # Get date
         try:
